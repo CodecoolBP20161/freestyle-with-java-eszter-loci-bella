e82a2365a2fc83423edc6b938309236a4c682f6a
eszterkaloz <eszterkaloz@gmail.com>
2016-11-03 15:08:00 +0100

    Implemented abstract and simple versions of paint programme

--- src/paint/SuperPaint.java
+++ src/paint/SuperPaint.java
@@ -7,58 +7,86 @@ import java.awt.event.*;
 public class SuperPaint extends JFrame {
 
     Color penColor = Color.black;
+    boolean abstractON = false;
+
 
     public SuperPaint() {
-        // Define the defaults for the JFrame
 
+    // Define the defaults for the JFrame
         this.setSize(1200, 700);
         this.setTitle("Super Paint");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    // Swing box and JPanel that will hold all the buttons
         JPanel buttonPanel = new JPanel();
-
-        // Swing box that will hold all the buttons
 
         Box buttonBox = Box.createVerticalBox();
 
-        // Button for creating a new canvas
+    // Button and method for creating a new canvas
         JButton newCanvasButton = new JButton("Create new");
 
         newCanvasButton.addActionListener(new ActionListener() {
-
             public void actionPerformed(ActionEvent e) {
-
                 repaint();
             }
         });
 
-        //  Button for choosing colors
+    //  Button for choosing colors
         JButton colorButton = this.colorPicker();
 
 
-        // Add the buttons to the box
+    // Buttons for abstract or simple version of paint
+        JButton simplePaintButton = chooseSimple();
+        JButton abstractPaintButton = chooseAbstract();
+
+    // Add the buttons to the box
         buttonBox.add(newCanvasButton);
         buttonBox.add(colorButton);
+        buttonBox.add(simplePaintButton);
+        buttonBox.add(abstractPaintButton);
 
 
-        // Add the box of buttons to the panel
+    // Add the box of buttons to the panel
 
         buttonPanel.add(buttonBox);
         buttonPanel.setBackground(Color.DARK_GRAY);
-
-        // Position the buttons on the left side of the frame
-
-        this.add(buttonPanel, BorderLayout.WEST);
-
-        // Make the drawing area take up the rest of the frame
-
         this.add(new DrawingBoard(), BorderLayout.CENTER);
 
-        // Show the frame and disable resizing option
+    // Position the buttons on the left side of the frame
+        this.add(buttonPanel, BorderLayout.WEST);
+
+
+    // Show the frame and disable resizing option
 
         this.setVisible(true);
         this.setResizable(false);
     }
+
+
+    public JButton chooseSimple(){
+        JButton button = new JButton("Simple paint");
+        button.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e) {
+                abstractON = false;
+            }
+        });
+        return button;
+    };
+
+    public JButton chooseAbstract(){
+        JButton button = new JButton("Abstract paint");
+        button.addMouseListener(new MouseAdapter() {
+
+            public void mouseClicked(MouseEvent e) {
+                abstractON = true;
+            }
+        });
+
+        return button;
+    };
+
+
 
     public JButton colorPicker() {
         JButton colorPicker = new JButton("Choose a color");
@@ -66,7 +94,6 @@ public class SuperPaint extends JFrame {
         colorPicker.addActionListener(new ActionListener() {
 
             public void actionPerformed(ActionEvent e) {
-
                 penColor = JColorChooser.showDialog(null, "Pick a color", penColor);
             }
         });
@@ -75,20 +102,26 @@ public class SuperPaint extends JFrame {
 
     private class DrawingBoard extends JComponent {
 
+        Point drawEnd, drawStart;
 
-        Point drawStart, drawEnd;
-
-        // Monitors events on the drawing area of the frame
 
         public DrawingBoard() {
-            this.addMouseListener(new MouseAdapter() {
-                                      @Override
-                                      public void mousePressed(MouseEvent e) {
 
-                // When the mouse is pressed get x & y position
-                drawStart = new Point(e.getX(), e.getY());
-                drawEnd = drawStart;
-            }
+            this.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent e) {
+
+                    if(!(abstractON)){
+
+                // get pos of x and y in case of simple painting
+                        drawEnd = new Point(e.getX(), e.getY());
+
+                    } else {
+                // get pos of x and y in case of abstract painting
+                        drawStart = new Point(e.getX(), e.getY());
+                        drawEnd = drawStart;
+                    }
+                }
             });
 
             this.addMouseMotionListener(new MouseMotionAdapter() {
@@ -96,16 +129,17 @@ public class SuperPaint extends JFrame {
                 public void mouseDragged(MouseEvent e) {
                     Graphics g = getGraphics();
                     g.setColor(penColor);
-                    g.drawLine(drawStart.x, drawStart.y, e.getX(), e.getY());
-                    drawEnd = new Point(e.getX(), e.getY());
-//                    g.dispose();
-//                    repaint();
+
+                    if (!(abstractON)){
+                        g.drawLine(drawEnd.x, drawEnd.y, e.getX(), e.getY());
+                        drawEnd = new Point(e.getX(), e.getY());}
+                    else {
+                        g.drawLine(drawStart.x, drawStart.y, e.getX(), e.getY());
+                        drawEnd = new Point(e.getX(), e.getY());}
                 }
             });
-        };
-
+        }
     }
-
     public static void main(String [] args) {
         new SuperPaint();
     }
