fab950027d077652ed137eb379728c484e1b4b4f
eszterkaloz <eszterkaloz@gmail.com>
2016-11-04 10:11:23 +0100

    implemented separate class for drawing board

---
+++ src/paint/DrawingBoard.java
@@ -0,0 +1,49 @@
+package paint;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class DrawingBoard extends JPanel {
+
+    Point drawEnd, drawStart;
+
+    public DrawingBoard() {
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+                if (!(SuperPaint.abstractON)) {
+
+                    // get pos of x and y in case of simple painting
+                    drawEnd = new Point(e.getX(), e.getY());
+
+                } else {
+                    // get pos of x and y in case of abstract painting
+                    drawStart = new Point(e.getX(), e.getY());
+                    drawEnd = drawStart;
+                }
+            }
+        });
+
+        this.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Graphics g = getGraphics();
+                g.setColor(SuperPaint.penColor);
+
+                if (!(SuperPaint.abstractON)) {
+                    g.drawLine(drawEnd.x, drawEnd.y, e.getX(), e.getY());
+                    drawEnd = new Point(e.getX(), e.getY());
+                } else {
+                    g.drawLine(drawStart.x, drawStart.y, e.getX(), e.getY());
+                    drawEnd = new Point(e.getX(), e.getY());
+                }
+            }
+        });
+    }
+
+        public static void main(String [] args) {};
+}
